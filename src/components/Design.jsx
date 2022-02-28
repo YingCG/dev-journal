c4dddcd375f1922ac31ffcfd795b2c37715161cf
YingCG <yingchiugoh@gmail.com>
2022-02-28 22:24:05 +1300

    prepare for heroku

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React from 'react'
 
 function Design() {
-    
-  return (
-    <>
-        <div className='main-content'>
-        </div>
-    </>
-  );
+    return (
+        <>
+            <div className='main-container'>
+                <div className='design'>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Environment</h3>
+                </div>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Jubilee Daily</h3>
+                </div>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Tourism</h3>
+                </div>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Interior Graphic</h3>
+                </div>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Food</h3>
+                </div>
+                <div className='card'>
+                    <img className='session-image' src='/images/photo-1487956382158-bb926046304a.jpg' alt='walkingimage' />
+                    <h3>Art Class</h3>
+                </div>
+            </div>
+            </div>
+        </>
+    )
 }
 
-export default Design;
+export default Design
