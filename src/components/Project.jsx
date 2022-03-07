55d1bd159f865ccb21ea1a89ca22048a25dcf00b
YingCG <ying@DESKTOP-VQDFUEH.localdomain>
2022-03-07 17:36:55 +1300

    fix style according to wireframe

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -17,7 +17,8 @@ function Project(){
               <img className='project-img' src='/images/EyeCandyCinema.jpg' alt='website interface'  width="100%" height="auto"/>
               {/* <iframe title='HTML | CSS' src="https://yingcg.github.io/EyeCandyCinema/" width="100%" height="300" /> */}
               </a>
-            <div className="case-title double-line" >Eye Candy Cinema</div>
+            <div className="case-title double-line" > HTML | CSS</div>
+            <p> My first website to demo basic building block of the Web to defines the meaning and structure of web content. </p>
             </div>
           </div>
           <div className="case-study">
@@ -25,7 +26,8 @@ function Project(){
             <a href="https://yingcg.github.io/retouchbarn/" target="_blank" rel="noreferrer">
             <img className='project-img' src='/images/RetouchBarn.jpg' alt='website interface'  width="100%" height="auto"/>
               </a>
-            <div className="case-title double-line">Retouch Barn</div>
+            <div className="case-title double-line"> HTML | CSS | JavaScript</div>
+            <p> This is a responsive web development </p>
          </div>
           </div>
           <div className="case-study">
@@ -33,21 +35,22 @@ function Project(){
             <a href="http://photoimagecbd.herokuapp.com/" target="_blank" rel="noreferrer">
             <img className='project-img' src='/images/photoimagecbd.jpg' alt='website interface'  width="100%" height="auto"/>
             </a>
-            <div className="case-title double-line">Photo Image</div>
+            <div className="case-title double-line">ReactJs | Express.js | CSS |  Heroku</div>
+            <p> An React Application that using JavaScript library building with component-based. This help me to understand object-oriented design.</p>
               </div>
           </div>
           <div className="case-study">
             <div className="project" width="100%" height="auto">
             <Link className='project-img' exact to='/projects/calculator'><Calculator/>
             </Link>
-            <div className="case-title double-line">Calculator</div>
+            <div className="case-title double-line">ReactJS | Redux | CSS </div>
               </div>
           </div>
           <div className="case-study">
             <div className="project" width="100%" height="auto">
             <Link className='project-img' exact to='/projects/todolist'><TodoList/>
             </Link>
-            <div className="case-title double-line">To-do List</div>
+            <div className="case-title double-line">ReactHook | CSS </div>
               </div>
           </div>
         </div>
