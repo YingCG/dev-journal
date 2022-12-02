e9f691d9bd47e37d815f96a1635fe440c5bc2f8b
Julian Adler <julian.adler@thldigital.com>
2022-12-02 21:45:37 +1300

    hash router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Css from "./ProjectsRoadMap/Css";
 import JsBasic from "./ProjectsRoadMap/JsBasic";
 import Tools from "./Tools/Tools";
