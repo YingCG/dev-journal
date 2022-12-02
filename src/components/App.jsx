57531e2efb88015c1b710680d8c7087bfa19b3d4
Ying Chiu Goh <YingG@whiteclife.ac.nz>
2022-12-02 16:20:20 +1300

    github pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Css from "./ProjectsRoadMap/Css";
 import JsBasic from "./ProjectsRoadMap/JsBasic";
 import Tools from "./Tools/Tools";
-import './style/style.css'
-import './style/button.css'
+import "./style/style.css";
+import "./style/button.css";
 import Design from "./HomePage/Design";
 import Hello from "./HomePage/Hello";
 import Projects from "./Projects/Projects";
@@ -14,30 +14,35 @@ import Calculator from "./Calculator/Calculator";
 import Home from "./HomePage/Home";
 import Pseudocode from "./Stories/Pseudocode";
 
-
 function App() {
-  
-    
   return (
-      <>
-        <div className="app-container">
+    <>
+      <div className="app-container">
         <Router>
           <Routes>
-            <Route exact path='/' element={<Home/>} />
-            <Route exact path='/design' element={<Design/>} />
-            <Route exact path='/development' element={<Projects/>} />
-            <Route exact path='/projects/css' element={<Css/>} />
-            <Route exact path='/projects/javascript-basic' element={<JsBasic/>} />
-            <Route exact path='/projects/tools' element={<Tools/>} />
-            <Route exact path='/projects/calculator' element={<Calculator/>} />
-            <Route exact path='/projects/shapes-gallery' element={<TimedShapes/>} />
-            <Route exact path='/projects/todolist' element={<TodoList/>} />
-            <Route exact path='/hello' element={<Hello />} />
-            <Route exact path='/stories' element={<Pseudocode />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/design" element={<Design />} />
+            <Route exact path="/development" element={<Projects />} />
+            <Route exact path="/projects/css" element={<Css />} />
+            <Route
+              exact
+              path="/projects/javascript-basic"
+              element={<JsBasic />}
+            />
+            <Route exact path="/projects/tools" element={<Tools />} />
+            <Route exact path="/projects/calculator" element={<Calculator />} />
+            <Route
+              exact
+              path="/projects/shapes-gallery"
+              element={<TimedShapes />}
+            />
+            <Route exact path="/projects/todolist" element={<TodoList />} />
+            <Route exact path="/hello" element={<Hello />} />
+            <Route exact path="/stories" element={<Pseudocode />} />
           </Routes>
         </Router>
-        </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
